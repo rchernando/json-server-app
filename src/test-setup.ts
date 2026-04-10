d66625da6c98a731775569f9b76b5124c54a2524
Roberto Hernando <roberto.carlos.hernando@gmail.com>
2026-04-10 12:26:00 +0200

    test: add Vitest configuration and test setup

---
+++ src/test-setup.ts
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom/vitest';
+import { afterEach } from 'vitest';
+import 'zone.js';
+import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+
+afterEach(() => {
+  getTestBed().resetTestingModule();
+});
